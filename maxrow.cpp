ad684005bdc9d9cc58ace625c9803a375ec4efdf
raghavkhanna12 <96764878+raghavkhanna12@users.noreply.github.com>
2024-02-23 15:52:13 +0530

    max in each row

---
+++ maxrow.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[3][3];
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int max=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(max<arr[i][j]){
+                max=arr[i][j];
+            }
+        }
+        cout<<max<<endl;
+    }
+}
