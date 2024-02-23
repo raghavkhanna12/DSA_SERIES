76530ef2f2b3a6cf819227083b22a26317a24ecb
raghavkhanna12 <96764878+raghavkhanna12@users.noreply.github.com>
2024-02-23 15:54:29 +0530

    1 change

--- maxrow.cpp
+++ maxrow.cpp
@@ -10,8 +10,9 @@ int main(){
         }
     }
 
-    int max=0;
+
     for(int i=0;i<3;i++){
+            int max=0;
         for(int j=0;j<3;j++){
             if(max<arr[i][j]){
                 max=arr[i][j];
