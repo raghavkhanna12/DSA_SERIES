fda143110beaf0451dee05ed3b90e70ea2a6a2c8
raghavkhanna12 <96764878+raghavkhanna12@users.noreply.github.com>
2024-02-25 10:32:45 +0530

    deletion added

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -27,6 +27,50 @@ Node* InsertIntoBST(Node* root,int data){
         root->left=InsertIntoBST(root->left,data);
     }
 }
+Node* minvalue(Node* root){
+    Node* temp=root;
+    while(temp!=NULL){
+        temp=temp->left;
+    }
+    return temp;
+}
+
+Node* DeleteFromBST(Node* root,int x){
+    
+    if(root==NULL){
+        return root;
+    }
+    if(root->data==x){
+         if(root->left==NULL && root->right==NULL){
+            delete(root);
+            return NULL;
+         }
+
+         if(root->left!=NULL && root->right==NULL){
+            Node* temp=root->left;
+            delete(root);
+            return temp;
+         }
+         if(root->left==NULL && root->right!=NULL){
+            Node* temp=root->right;
+            delete(root);
+            return temp;
+         }
+
+         if(root->left!=NULL && root->right!=NULL){
+            int mini=minvalue(root->right)->data;
+            root->data=mini;
+            DeleteFromBST(root->right,x);
+         }
+    }
+
+    if(x>root->data){
+        root->right=DeleteFromBST(root->right,x);
+    }
+    else{
+        root->left=DeleteFromBST(root->left,x);
+    }
+}
 
 void inorder(Node* root){
     if(root==NULL){
@@ -42,5 +86,8 @@ void inorder(Node* root){
         InsertIntoBST(root,30);
         InsertIntoBST(root,5);
         InsertIntoBST(root,50);
+        inorder(root);
+        cout<<endl;
+        DeleteFromBST(root,5);
         inorder(root);
     }
