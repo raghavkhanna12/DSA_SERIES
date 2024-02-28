1bb42772dc61dd81ddb5276569e2ba450ed55d73
raghavkhanna12 <96764878+raghavkhanna12@users.noreply.github.com>
2024-02-28 15:29:52 +0530

    heapify added

--- HeapImplementation.cpp
+++ HeapImplementation.cpp
@@ -61,6 +61,24 @@ class heap{
 
 };
 
+void heapify(int arr[],int n,int i){
+int largest=i;
+int left=2*i;
+int right=2*i+1;
+
+if(left<n && arr[largest]<arr[left]){
+    largest=left;
+}
+
+if(right<n && arr[largest]<arr[right]){
+    largest=right;
+}
+
+if(largest!=i){
+    swap(arr[largest],arr[i]);
+    heapify(arr,n,largest);
+}
+}
 int main(){
     heap h;
     
@@ -70,5 +88,14 @@ int main(){
     h.insert(52);
     h.insert(54);
     h.deletefromheap();
-    h.print();
+
+    int arr[6]={-1,54,53,55,52,60};
+    int n=5;
+    for(int i=n/2;i>0;i--){
+        heapify(arr,n,i);
+    }
+    for(int i=1;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+   // h.print();
 }
