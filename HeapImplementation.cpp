817d67983489fcdae86cc7d65490c49f457be1e2
raghavkhanna12 <96764878+raghavkhanna12@users.noreply.github.com>
2024-02-28 15:46:34 +0530

    heap sort added

--- HeapImplementation.cpp
+++ HeapImplementation.cpp
@@ -66,11 +66,11 @@ int largest=i;
 int left=2*i;
 int right=2*i+1;
 
-if(left<n && arr[largest]<arr[left]){
+if(left<=n && arr[largest]<arr[left]){
     largest=left;
 }
 
-if(right<n && arr[largest]<arr[right]){
+if(right<=n && arr[largest]<arr[right]){
     largest=right;
 }
 
@@ -78,6 +78,17 @@ if(largest!=i){
     swap(arr[largest],arr[i]);
     heapify(arr,n,largest);
 }
+}
+
+
+
+void sortheap(int arr[],int n){
+     int size=n;
+     while(size>1){
+    swap(arr[size],arr[1]);
+    size--;
+    heapify(arr,size,1);
+     }
 }
 int main(){
     heap h;
@@ -89,12 +100,17 @@ int main(){
     h.insert(54);
     h.deletefromheap();
 
-    int arr[6]={-1,54,53,55,52,60};
+    int arr[6]={-1,54,53,55,52,50};
     int n=5;
     for(int i=n/2;i>0;i--){
         heapify(arr,n,i);
     }
-    for(int i=1;i<n;i++){
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    sortheap(arr,n);
+     for(int i=1;i<=n;i++){
         cout<<arr[i]<<" ";
     }
    // h.print();
