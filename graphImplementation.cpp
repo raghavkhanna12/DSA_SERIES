049f98bbfdb8506d3830ae3f0c50a3a6f090623c
raghavkhanna12 <96764878+raghavkhanna12@users.noreply.github.com>
2024-03-04 13:05:55 +0530

    Update graphImplementation.cpp

--- graphImplementation.cpp
+++ graphImplementation.cpp
@@ -127,6 +127,53 @@ bool cycledfs(unordered_map<int,bool>&visited,vector<int>* adj,int node,int pare
 	    }
 	    return ans;
 	}
+
+vector<int> shortestPath(vector<pair<int, int>> edges, int n, int m, int s,int t) {
+              
+	unordered_map<int,list<int>>adj;
+    for(int i=0;i<edges.size();i++){
+        int u=edges[i].first;
+        int v=edges[i].second;
+        adj[u].push_back(v);
+		adj[v].push_back(u);
+    }
+
+    unordered_map<int,bool>visited;
+    unordered_map<int,int>parent;
+
+    queue<int>q;
+    q.push(s);
+	parent[s]=-1;
+    visited[s]=true;
+    
+    while(!q.empty())
+    {
+        int frontNode=q.front();
+        q.pop();
+        visited[frontNode]=true;
+
+        for( auto i:adj[frontNode]){
+            if(!visited[i]){
+                visited[i]=true;
+                parent[i]=frontNode;
+			q.push(i);
+            }
+        }
+
+    }
+
+    vector<int>ans;
+
+    int current=t;
+	ans.push_back(t);
+    while(current!=s){
+        current=parent[current];
+        ans.push_back(current);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+
+	}
     
 
 
